965e765b3ca3a4c3e0c2793290737518aaab8644
Pablo Copes <pcopescolombo@alumnos.exa.unicen.edu.ar>
2022-11-05 13:50:08 -0300

    error de router fixed, falta arreglar tema de postman con order&sort

--- tpRudaApi/api-router.php
+++ tpRudaApi/api-router.php
@@ -6,7 +6,7 @@ $router = new Router();
 $router->addRoute('beers', 'GET', 'BeerApiController', 'getBeers');
 $router->addRoute('beers/:ID', 'GET', 'BeerApiController', 'getBeer');
 $router->addRoute('beers', 'POST', 'BeerApiController', 'addBeer');
-$router->addRoute('beers/:ID', 'DELETE', 'BeerApiCongittroller', 'deleteBeer');
+$router->addRoute('beers/:ID', 'DELETE', 'BeerApiController', 'deleteBeer');
 $router->addRoute('beers/:ID', 'PUT', 'BeerApiController', 'updateBeer');
 $router->addRoute('beers', 'GET', 'BeerApiController', 'getOrdered');
 
